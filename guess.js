9c571323a9e570e28d2efb451be4f5b42944cc60
Abdelilah Naciri <abnaciri@e2r2p1.1337.ma>
2024-05-05 18:49:57 +0100

    Start Guess Game

---
+++ guess.js
@@ -0,0 +1,4 @@
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Game Created By Elzero`;
